0081ae4385e5a44b2f200e1f2fbdbb5178b9db25
Rafaella Barbosa <rafaellacsta@hotmail.com>
2025-09-16 14:32:29 -0300

    feat: implementar componente Keyboard com organização de botões e suporte a operações

--- lib/componets/keyboard.dart
+++ lib/componets/keyboard.dart
@@ -1,56 +1,55 @@
-import 'package:calculator/componets/button.dart';
-import 'package:calculator/componets/button_row.dart';
 import 'package:flutter/material.dart';
+
+import 'button.dart';
+import 'button_row.dart';
 
 class Keyboard extends StatelessWidget {
   final void Function(String) onPressed;
-  const Keyboard({super.key, required this.onPressed});
+
+  const Keyboard(this.onPressed, {super.key});
 
   @override
   Widget build(BuildContext context) {
     return SizedBox(
       height: 500,
       child: Column(
-        children: [
-          ButtonRow(
-            buttons: [
-              Button.big(text: "AC", onPressed: onPressed),
-              Button(text: "+/-", onPressed: onPressed),
-              Button(text: "%", onPressed: onPressed),
-              Button(text: "÷", onPressed: onPressed),
-            ],
-          ),
-          ButtonRow(
-            buttons: [
-              Button(text: "7", onPressed: onPressed),
-              Button(text: "8", onPressed: onPressed),
-              Button(text: "9", onPressed: onPressed),
-              Button(text: "x", onPressed: onPressed),
-            ],
-          ),
-          ButtonRow(
-            buttons: [
-              Button(text: "4", onPressed: onPressed),
-              Button(text: "5", onPressed: onPressed),
-              Button(text: "6", onPressed: onPressed),
-              Button(text: "-", onPressed: onPressed),
-            ],
-          ),
-          ButtonRow(
-            buttons: [
-              Button(text: "1", onPressed: onPressed),
-              Button(text: "2", onPressed: onPressed),
-              Button(text: "3", onPressed: onPressed),
-              Button(text: "+", onPressed: onPressed),
-            ],
-          ),
-          ButtonRow(
-            buttons: [
-              Button.big(text: "0", onPressed: onPressed),
-              Button(text: ",", onPressed: onPressed),
-              Button(text: "=", onPressed: onPressed),
-            ],
-          ),
+        children: <Widget>[
+          ButtonRow([
+            Button.big(
+              text: 'AC',
+              color: Button.darkColor,
+              onPressed: onPressed,
+            ),
+            Button(text: '%', color: Button.darkColor, onPressed: onPressed),
+            Button.operation(text: '/', onPressed: onPressed),
+          ]),
+          const SizedBox(height: 1),
+          ButtonRow([
+            Button(text: '7', onPressed: onPressed),
+            Button(text: '8', onPressed: onPressed),
+            Button(text: '9', onPressed: onPressed),
+            Button.operation(text: 'x', onPressed: onPressed),
+          ]),
+          const SizedBox(height: 1),
+          ButtonRow([
+            Button(text: '4', onPressed: onPressed),
+            Button(text: '5', onPressed: onPressed),
+            Button(text: '6', onPressed: onPressed),
+            Button.operation(text: '-', onPressed: onPressed),
+          ]),
+          const SizedBox(height: 1),
+          ButtonRow([
+            Button(text: '1', onPressed: onPressed),
+            Button(text: '2', onPressed: onPressed),
+            Button(text: '3', onPressed: onPressed),
+            Button.operation(text: '+', onPressed: onPressed),
+          ]),
+          const SizedBox(height: 1),
+          ButtonRow([
+            Button.big(text: '0', onPressed: onPressed),
+            Button(text: '.', onPressed: onPressed),
+            Button.operation(text: '=', onPressed: onPressed),
+          ]),
         ],
       ),
     );
