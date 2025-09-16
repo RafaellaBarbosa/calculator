8cfc88d97afaa64d81b963e1143f853c9a2e0d48
Rafaella Barbosa <rafaellacsta@hotmail.com>
2025-09-16 14:34:32 -0300

    feat: integrar lógica da Memory com UI no componente Calculator

--- lib/screens/calculator.dart
+++ lib/screens/calculator.dart
@@ -1,37 +1,32 @@
 import 'package:calculator/componets/display.dart';
 import 'package:calculator/componets/keyboard.dart';
+import 'package:calculator/models/memory.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
 class Calculator extends StatefulWidget {
   const Calculator({super.key});
 
   @override
-  State<Calculator> createState() => _CalculatorState();
+  CalculatorState createState() => CalculatorState();
 }
 
-class _CalculatorState extends State<Calculator> {
-  String _displayText = '0';
+class CalculatorState extends State<Calculator> {
+  final Memory memory = Memory();
 
-  void _onPressed(String value) {
+  _onPressed(String command) {
     setState(() {
-      if (_displayText == '0' && value != 'AC') {
-        _displayText = value;
-      } else if (value == 'AC') {
-        _displayText = '0';
-      } else {
-        _displayText += value;
-      }
+      memory.applyCommand(command);
     });
   }
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Column(
-        children: [
-          Display(text: _displayText),
-          Keyboard(onPressed: _onPressed),
-        ],
+    SystemChrome.setPreferredOrientations([DeviceOrientation.portraitUp]);
+
+    return MaterialApp(
+      home: Column(
+        children: <Widget>[Display(memory.value), Keyboard(_onPressed)],
       ),
     );
   }
